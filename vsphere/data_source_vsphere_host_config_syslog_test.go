472492bafd129a1eeb7191c9c4ef6d8f84cbfde6
Travis Shanks <travis.shanks@encore.tech>
2024-01-04 14:30:38 -0500

    - Update data source host config syslog tests

--- vsphere/data_source_vsphere_host_config_syslog_test.go
+++ vsphere/data_source_vsphere_host_config_syslog_test.go
@@ -10,54 +10,46 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/hashicorp/terraform-provider-vsphere/vsphere/internal/helper/hostservicestate"
-	"github.com/hashicorp/terraform-provider-vsphere/vsphere/internal/helper/provider"
 	"github.com/hashicorp/terraform-provider-vsphere/vsphere/internal/helper/testhelper"
-	"github.com/vmware/govmomi/vim25/types"
 )
 
 func TestAccDataSourceVSphereHostConfigSyslog_basic(t *testing.T) {
-	resourceName := "data.vsphere_host_service_state.h1"
-	policy := types.HostServicePolicyOn
+	resourceName := "data.vsphere_host_config_syslog.h1"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			RunSweepers()
 			testAccPreCheck(t)
+			testAccCheckEnvVariablesF(t, []string{"ESXI_LOG_HOST"})
 		},
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceVSphereHostConfigSyslogConfig(policy),
+				Config: testAccDataSourceVSphereHostConfigSyslogConfig(),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestMatchResourceAttr(
 						resourceName,
 						"id",
 						regexp.MustCompile("^host-"),
 					),
-					testAccDataSourceVSphereHostConfigSyslogDataCheck(resourceName, policy),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataSourceVSphereHostConfigSyslogConfig(policy types.HostServicePolicy) string {
+func testAccDataSourceVSphereHostConfigSyslogConfig() string {
 	return fmt.Sprintf(
 		`
 		%s
 
-		resource "vsphere_host_service_state" "h1" {
+		resource "vsphere_host_config_syslog" "h1" {
 			host_system_id = data.vsphere_host.roothost1.id
-			service {
-				key = "%s"
-				policy = "%s"
-			}
+			log_host = "%s"
 		}
 
-		data "vsphere_host_service_state" "h1" {
-			host_system_id = vsphere_host_service_state.h1.id
+		data "vsphere_host_config_syslog" "h1" {
+			host_system_id = vsphere_host_config_syslog.h1.id
 		}
 		`,
 		testhelper.CombineConfigs(
@@ -65,37 +57,6 @@ func testAccDataSourceVSphereHostConfigSyslogConfig(policy types.HostServicePoli
 			testhelper.ConfigDataRootComputeCluster1(),
 			testhelper.ConfigDataRootHost1(),
 		),
-		os.Getenv("TF_VAR_VSPHERE_SERVICE_KEY_1"),
-		policy,
+		os.Getenv("ESXI_LOG_HOST"),
 	)
-}
-
-func testAccDataSourceVSphereHostConfigSyslogDataCheck(resourceName string, policy types.HostServicePolicy) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[resourceName]
-
-		if !ok {
-			return fmt.Errorf("%s key not found on the server", resourceName)
-		}
-
-		client := testAccProvider.Meta().(*Client).vimClient
-
-		hsList, err := hostservicestate.GetHostServies(client, rs.Primary.ID, provider.DefaultAPITimeout)
-		if err != nil {
-			return fmt.Errorf("error trying to get host services from host '%s'", err)
-		}
-
-		for _, hs := range hsList {
-			if hs.Key == os.Getenv("TF_VAR_VSPHERE_SERVICE_KEY_1") {
-				if !hs.Running {
-					return fmt.Errorf("service '%s' is not running", os.Getenv("TF_VAR_VSPHERE_SERVICE_KEY_1"))
-				}
-				if hs.Policy != string(policy) {
-					return fmt.Errorf("service '%s' does not have policy '%s'; got '%s'", hs.Key, policy, hs.Policy)
-				}
-			}
-		}
-
-		return nil
-	}
 }
